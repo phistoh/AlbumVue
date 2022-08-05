a01cfd7ae78875751f299411f7c4a5a989ec4600
Moritz <gobbert@uni-trier.de>
2022-08-05 11:34:48 +0200

    New tutorial -> vuemastery.com

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'Comfy green socks',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.de'
+        }
+    }
+})
