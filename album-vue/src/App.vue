c7662d58f326aa9acd344399b2d1a7f1fddd9138
Moritz <gobbert@uni-trier.de>
2022-08-08 16:53:45 +0200

    SFC setup and some small table tests

---
+++ album-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <AlbumList/>
+</template>
+
+<script>
+import AlbumList from './components/AlbumList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AlbumList
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ album-vue/src/components/AlbumList.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+  data() {
+    return {
+      albums: [
+            { artist: "A Forest Of Stars", album: "Grave Mounds and Grave Mistakes", mediatype: "cd" },
+            { artist: "Casper", album: "Hinterland", mediatype: "cd" },
+            { artist: "Black Kirin", album: "秦淮", mediatype: "digital" },
+            { artist: "The Ruins of Beverast", album: "Exuvia", mediatype: "vinyl" },
+            { artist: "Bring Me the Horizon", album: "Post Human: Survival Horror", mediatype: "tape" }
+        ]
+    }
+  },
+  methods: {
+    sort() {
+
+    }
+  }
+}
+</script>
+
+<template>
+    <table>
+    <thead>
+        <th @click="sort('artist')">Artist</th>
+        <th @click="sort('album')">Album</th>
+        <th @click="sort('mediatype')">Type</th>
+    </thead>
+    <tbody>
+        <tr v-for="album in albums" :key="album.id">
+            <td>{{album.artist}}</td>
+            <td>{{album.album}}</td>
+            <td>{{album.mediatype}}</td>
+        </tr>
+    </tbody>
+    </table>
+</template>
+
+<style>
+</style>
